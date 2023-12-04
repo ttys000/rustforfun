6b0b4bd345a739038500a20a7763e96e4df1ff3d
lipengyux <lipengyux@gmail.com>
2023-12-04 11:21:35 +0800

    [chapter-2] more rustic implementation and integration test

--- src/bin/echo.rs
+++ src/bin/echo.rs
@@ -22,7 +22,10 @@ fn main() {
         )
         .get_matches();
 
-    println!("{:#?}", matches);
+    let text = matches.values_of_lossy("text").unwrap();
+    let omit_newline = matches.is_present("omit_newline");
+    let ending = if omit_newline { "" } else { "\n" };
+    print!("{}{}", text.join(" "), ending);
 }
 
 #[allow(dead_code)]
--- tests/cli.rs
+++ tests/cli.rs
@@ -17,39 +17,3 @@ fn false_not_ok() {
     let mut cmd = Command::cargo_bin("false").unwrap();
     cmd.assert().failure();
 }
-
-#[test]
-fn echo_ok() {
-    let mut cmd = Command::cargo_bin("echo").unwrap();
-    cmd.assert().success().stdout("\n");
-
-    let mut cmd = Command::cargo_bin("echo").unwrap();
-    cmd.args(["-n"]).assert().success().stdout("");
-
-    let mut cmd = Command::cargo_bin("echo").unwrap();
-    cmd.args(["hello"]).assert().success().stdout("hello\n");
-
-    let mut cmd = Command::cargo_bin("echo").unwrap();
-    cmd.args(["hello", "world"])
-        .assert()
-        .success()
-        .stdout("hello world\n");
-
-    let mut cmd = Command::cargo_bin("echo").unwrap();
-    cmd.args(["hello", "world", "hello     RUST"])
-        .assert()
-        .success()
-        .stdout("hello world hello     RUST\n");
-
-    let mut cmd = Command::cargo_bin("echo").unwrap();
-    cmd.args(["-n", "hello", "world", "hello     RUST"])
-        .assert()
-        .success()
-        .stdout("hello world hello     RUST");
-
-    let mut cmd = Command::cargo_bin("echo").unwrap();
-    cmd.args(["hello", "world", "-n", "hello     RUST"])
-        .assert()
-        .success()
-        .stdout("hello world -n hello     RUST\n");
-}
